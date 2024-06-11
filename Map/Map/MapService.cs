c3f677e4b3eb2a4ef61f816debb5a825be4ecdca
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2024-06-11 22:32:47 +0200

    14 reverse

--- Map/Map/MapService.cs
+++ Map/Map/MapService.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+//using Windows.Devices.Geolocation;
+using System.Configuration;
 
 namespace TourPlanner.BusinessLogic.Map
 {
