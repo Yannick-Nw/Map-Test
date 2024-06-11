d2340c3f1e1715433834f10f18abd9a4cf48ccbb
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2024-06-12 01:41:17 +0200

    24 start

--- Map/Map/Program.cs
+++ Map/Map/Program.cs
@@ -2,7 +2,7 @@
 
 using TourPlanner.BusinessLogic.Map;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Map-Test Start...");
 MapService mapService = new MapService();
 string filePath = await mapService.GetMap("Höchstädtpl. 6, 1200 Wien", "Universitätsring 1, 1010 Wien");
 Console.WriteLine(filePath);
